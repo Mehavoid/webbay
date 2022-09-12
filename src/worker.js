34419762032e193959b29d2a079f9ac51224528c
Mehavoid <tontole@riseup.net>
2022-09-12 23:51:55 +0300

    Implement base routing and validation

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,100 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router } from 'itty-router';
+import { validate } from '@cfworker/json-schema';
+
+const headers = Object.freeze({
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Max-Age': 86400,
+  'Content-Type': 'application/json; charset=UTF-8',
+});
+
+const SOURCE_SCHEMA = {
+  type: 'object',
+  required: ['source'],
+  properties: {
+    source: {
+      type: 'string',
+      enum: ['webshare'],
+    },
+  },
+};
+
+const buildURLSchema = (pattern) => ({
+  type: 'object',
+  required: ['url'],
+  properties: {
+    url: {
+      type: 'string',
+      pattern,
+    },
+  },
+});
+
+const R = Object.freeze({
+  krakenfiles: 'https?://(?:www.)?krakenfiles.com/view/(?<id>[^/&?]+)',
+  webshare: 'https?://(?:www|en.)?webshare.cz/(?:#/)?file/(?<id>[^/&?]+)',
+});
+
+const safeFormData = async (request) => {
+  try {
+    const formData = await request.formData();
+    return Object.fromEntries(formData);
+  } catch {
+    return null;
+  }
+};
+
+const isEmpty = (object) => {
+  for (const i in object) return false;
+  return true;
+};
+
+const router = new Router();
+
+router
+  .options('/api/:source', () => new Response(null, { status: 204, headers }))
+  .post('/api/:source', async (request) => {
+    const { params } = request;
+    const { valid: paramsValid } = validate(params, SOURCE_SCHEMA);
+    if (paramsValid === false)
+      return new Response(
+        JSON.stringify(
+          { status: 1, text: '404 Not Found' },
+          { status: 404, headers },
+        ),
+      );
+    const data = await safeFormData(request);
+    if (isEmpty(data))
+      return new Response(
+        JSON.stringify(
+          { status: 1, text: 'Request body is missing' },
+          { status: 400, headers },
+        ),
+      );
+    const schema = buildURLSchema(R[params.source]);
+    const { valid: URLValid } = validate(data, schema);
+    if (URLValid === false)
+      return new Response(
+        JSON.stringify(
+          { status: 1, text: 'The "url" parameter is missing or incorrect' },
+          { status: 400, headers },
+        ),
+      );
+    return new Response(JSON.stringify({ status: 0, result: URLValid }), {
+      status: 200,
+      headers,
+    });
+  })
+  .all(
+    '*',
+    () =>
+      new Response(JSON.stringify({ status: 1, text: '404 Not Found' }), {
+        status: 404,
+        headers,
+      }),
+  );
 
 export default {
-  async fetch() {
-    return new Response('Hello World!');
-  },
+  fetch: router.handle,
 };
